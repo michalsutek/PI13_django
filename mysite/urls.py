ba54dd2f502b2ac7bcfd447af4d94fbe27704ba4
Michal Šutek <sutek.spsit@gmail.com>
2024-02-09 11:46:21 +0100

    add app calc

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('skola/', include('skola.urls')),
+    path('calc/', include('calc.urls')),
 ]
